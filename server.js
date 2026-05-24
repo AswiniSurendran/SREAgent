41b2a45dde391b5b35b0e3cd65b776b82bd988af
Aswini Surendran <assurendran@microsoft.com>
2026-05-24 17:46:27 +0530

    Fix Node app for Azure

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-// serve index.html
+// Serve static files (index.html)
 app.use(express.static(__dirname));
 
+// Default route
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
+  console.log("App running on port " + port);
 });
